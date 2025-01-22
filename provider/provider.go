9f40ed8bc2e631811880ec76c085ea7d31276b0c
Manh <nguyenmanh180102@gmail.com>
2025-01-22 13:57:31 +0700

    fix docs

--- provider/provider.go
+++ provider/provider.go
@@ -3,18 +3,17 @@ package provider
 import (
 	"express_be/conf"
 	"express_be/core/db/postgresql"
-
-	"github.com/redis/go-redis/v9"
+	// "github.com/redis/go-redis/v9"
 )
 
 type AppProvider struct {
 	Postgres *postgresql.Database
-	Redis    *redis.Client
+	// Redis    *redis.Client
 }
 
 func NewAppProvider(config conf.ServiceConfig) *AppProvider {
 	return &AppProvider{
 		Postgres: ProvidePostgres(config),
-		Redis:    ProvideRedis(config),
+		// Redis:    ProvideRedis(config),
 	}
 }
